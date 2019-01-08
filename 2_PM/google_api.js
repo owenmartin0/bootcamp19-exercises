78132c232f32584012d371e7a4eb7d08884c9229
Owen Martin <owenmartin@college.harvard.edu>
2019-01-08 16:43:59 -0500

    Committing all

--- 2_PM/google_api.js
+++ 2_PM/google_api.js
@@ -1,1 +1,10 @@
 // TODO - create client object for Google API connection which can be exported
+
+const key = require('./config')
+
+const googleMapsClient = require('@google/maps').createClient({
+    key: key.apiKey,
+    Promise: Promise
+  });
+
+module.exports = googleMapsClient
--- 2_PM/placeinfo.js
+++ 2_PM/placeinfo.js
@@ -2,3 +2,21 @@
 / returns the following string, formatted with the appropriate values:
 
 'INPUTTED_PLACE is located at coordinates RETURNED_COORDINATES, at the address RETURNED_ADDRESS.' */
+const module1 = require('@google/maps')
+const key = require('./config')
+const googleMapsClient = require('./google_api')
+
+const INPUTTED_PLACE = 'Harvard'
+
+googleMapsClient.findPlace({input: INPUTTED_PLACE,
+inputtype: "textquery"}).asPromise()
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const RETURNED_COORDINATES = "yay"
+const RETURNED_ADDRESS = "huh"
+console.log(`\n\n${INPUTTED_PLACE} is located at coordinates ${RETURNED_COORDINATES}, at the address ${RETURNED_ADDRESS}.\n\n`)
